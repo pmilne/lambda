03b83c34e02d352bdcb150610b5f8ef47b71f769
pmilne <philip@pmilne.net>
2016-03-27 22:53:32 -0700

    Tidy.

--- src/lambda/Test.java
+++ src/lambda/Test.java
@@ -57,7 +57,7 @@ public class Test {
 
     public static void main(String[] args) {
 //        test("1 2", "1", "2");
-//        test("(1 2 (3 4))", "(1 2 (3 4))");
+//        test("(1 (2 3) ((4 5 6) 7))", "(1 2 (3 4))");
         test("(lambda (x) x)", "(lambda (a) a)");
         test("((lambda (x) x) 1)", "1");
         test("((lambda (x) x) (lambda (x) x))", "(lambda (a) a)");
