9b3414d2d7df656e99c9815bbf56bf21b624097c
pmilne <philip@pmilne.net>
2016-03-30 15:03:26 -0700

    Simplify.

--- src/lambda/Decompiler.java
+++ src/lambda/Decompiler.java
@@ -15,25 +15,26 @@ public class Decompiler {
         Expression getExp();
     }
 
-    public static Mole createMole(Expression exp, Converter converter) {
-        return new Mole() {
-            @Override
-            public Expression getExp() {
-                return exp;
-            }
-
-            public Primitive apply(Primitive a) {
-                return primitive(createMole(Expressions.CONSTRUCTOR.application(exp, converter.convert(a)), converter));
-            }
-        };
-    }
-
     public static abstract class Converter {
         public abstract Expression convert(Primitive o);
+        public abstract Mole createMole(Expression exp);
 
         public static Converter create(int level) {
             Expression.Visitor<Expression> c = Expressions.CONSTRUCTOR;
             return new Converter() {
+                public Mole createMole(Expression exp) {
+                    return new Mole() {
+                        @Override
+                        public Expression getExp() {
+                            return exp;
+                        }
+
+                        public Primitive apply(Primitive a) {
+                            return primitive(createMole(c.application(exp, convert(a))));
+                        }
+                    };
+                }
+
                 public Expression convert(Primitive o) {
                     return o.accept(new Primitive.Visitor<Expression>() {
                         @Override
@@ -53,8 +54,8 @@ public class Decompiler {
                             }
                             Expression var = c.symbol(varName(level));
                             Converter converter = create(level + 1);
-                            Primitive apply = f.apply(primitive(createMole(var, converter)));
-                            return c.lambda(var, converter.convert(apply));
+                            Primitive application = f.apply(primitive(converter.createMole(var)));
+                            return c.lambda(var, converter.convert(application));
                         }
                     });
                 }
