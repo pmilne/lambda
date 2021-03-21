f106d3b028bb0f9d37eb4f245c43f68a74ce8939
pmilne <philip@pmilne.net>
2021-03-21 12:44:55 +0000

    Tidy.

--- src/lambda/Evaluator.java
+++ src/lambda/Evaluator.java
@@ -13,7 +13,7 @@ public class Evaluator {
     }
 
     // This visitor turns symbols into numbers at 'compile' time and provides a mechanism for evaluation.
-    private static Expression.Visitor<Implementation> createCompiler(List<String> nameStack) {
+    private static Expression.Visitor<Implementation> createEvaluator(List<String> nameStack) {
         return new Expression.Visitor<Implementation>() {
                     @Override
                     public Implementation constant(Primitive value) {
@@ -35,13 +35,13 @@ public class Evaluator {
 
                     @Override
                     public Implementation lambda(String var, Expression exp) {
-                        Implementation exp0 = exp.accept(createCompiler(List.create(nameStack, var)));
+                        Implementation exp0 = exp.accept(createEvaluator(List.create(nameStack, var)));
                         return env -> Primitives.CONSTRUCTOR.function(arg -> exp0.eval(List.create(env, arg)));
                     }
                 };
     }
 
-    public static final Expression.Visitor<Implementation> COMPILER = createCompiler(new List<String>(null, null) {
+    public static final Expression.Visitor<Implementation> EVALUATOR = createEvaluator(new List<String>(null, null) {
         @Override
         public int indexOf(String name) {
             throw new RuntimeException("Undefined variable: " + name);
@@ -49,6 +49,6 @@ public class Evaluator {
     });
 
     public static Primitive eval(Expression input) {
-        return input.accept(COMPILER).eval(null);
+        return input.accept(EVALUATOR).eval(null);
     }
 }
